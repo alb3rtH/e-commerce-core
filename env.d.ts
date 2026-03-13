0a8365cc827e7e5e7f72cf98b9c83a6c1a98b7a2
n3s <n3s.creator@gmail.com>
2026-03-13 11:34:33 -0400

    feat: add docker and env configurations

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+declare global {
+  namespace NodeJS {
+    interface Env {
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+    }
+  }
+}
