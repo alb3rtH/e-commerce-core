937571df979c3142c9de6272258baee1b13b3103
alb3rth <alberthxd1@gmail.com>
2026-05-31 07:53:56 -0400

    FEAT: Filters & Interceptors error created and implemented app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
-import { databaseConf } from './config/database';
-import { OrdersModule } from './modules/order/order.module';
-import { ProductModule } from './modules/product/product.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filters';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { databaseConf } from './config/database';
 import { AuthModule } from './modules/auth/auth.module';
+import { OrdersModule } from './modules/order/order.module';
 import { PaymentModule } from './modules/payment/payment.module';
+import { ProductModule } from './modules/product/product.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +19,16 @@ import { PaymentModule } from './modules/payment/payment.module';
     ProductModule,
     AuthModule,
     PaymentModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
   ],
 })
 export class AppModule {}
